a5718420bd933f9a373f6836a0698b2c48dd43e7
Sophie Nichol <snichol00@stuy.edu>
2019-03-11 10:11:39 -0400

    added Quick method and partition

---
+++ Quick.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.lang.Math;
+
+public class Quick{
+  public static int partition(int [] data, int start, int end){
+    //choose random pivot
+    Random r = new Random();
+    int pIdx = r.nextInt(data.length);
+    int pVal = data[pIdx];
+
+    //swap pivot to be first int
+    data[pIdx] = data[0];
+    data[0] = pVal;
+
+    //once start and end are equal, array has been partitioned
+    while (start != end){
+      if (data[start] > pVal){
+        //swap start and end
+        int temp = data[start];
+        data[start] = data[end];
+        data[end] = temp;
+        end--;
+      }
+      else{
+        start++;
+      }
+    }
+    //once sorted, you need to switch the pivot back into place
+    if (data[start] > pVal){
+      data[0] = data[start - 1];
+      data[start - 1] = pVal;
+      return start - 1;
+    }
+    else{
+      data[0] = data[start];
+      data[start] = pVal;
+      return start;
+    }
+  }
+
+  public static int quickselect(int[] data, int k){
+    partition(data, 0, data.length - 1);
+    return data[k];
+  }
+}
