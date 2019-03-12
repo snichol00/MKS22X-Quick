472fbce2461c57055154dddc96a5ecde7b229aaa
Sophie Nichol <snichol00@stuy.edu>
2019-03-11 23:58:34 -0400

    added to check for multiples of numbers

--- Quick.java
+++ Quick.java
@@ -3,44 +3,59 @@ import java.util.Random;
 import java.lang.Math;
 
 public class Quick{
-  public static int partition(int [] data, int start, int end){
-    //choose random pivot
-    Random r = new Random();
-    int pIdx = r.nextInt(data.length);
-    int pVal = data[pIdx];
+  public class Partition{
+    public static int partition(int [] data, int start, int end){
+      //choose random pivot
+      Random r = new Random();
+      int pIdx = r.nextInt(data.length);
+      int pVal = data[pIdx];
 
-    //swap pivot to be first int
-    data[pIdx] = data[0];
-    data[0] = pVal;
-
-    //once start and end are equal, array has been partitioned
-    while (start != end){
+      //swap pivot to be first int
+      data[pIdx] = data[start];
+      data[start] = pVal;
+      //System.out.println(Arrays.toString(data));
+      //once start and end are equal, array has been partitioned
+      while (start != end){
+        if (data[start] > pVal){
+          //swap start and end
+          int temp = data[start];
+          data[start] = data[end];
+          data[end] = temp;
+          end--;
+        }
+        else{
+          start++;
+        }
+      }
+      //System.out.println("loop finished");
+      //System.out.println(Arrays.toString(data));
+      //once sorted, you need to switch the pivot back into place
       if (data[start] > pVal){
-        //swap start and end
-        int temp = data[start];
-        data[start] = data[end];
-        data[end] = temp;
-        end--;
+        data[0] = data[start - 1];
+        data[start - 1] = pVal;
+        return start - 1;
       }
       else{
-        start++;
+        data[0] = data[start];
+        data[start] = pVal;
+        return start;
       }
     }
-    //once sorted, you need to switch the pivot back into place
-    if (data[start] > pVal){
-      data[0] = data[start - 1];
-      data[start - 1] = pVal;
-      return start - 1;
-    }
-    else{
-      data[0] = data[start];
-      data[start] = pVal;
-      return start;
-    }
-  }
 
   public static int quickselect(int[] data, int k){
     partition(data, 0, data.length - 1);
-    return data[k];
+    int num = 1;
+    int prev = data[0];
+    for (int x = 1; x < data.length; x++){
+      if (num == k){
+        return data[x];
+      }
+      if (data[x] != prev){
+        num++;
+        prev = data[x];
+      }
+    }
+    return 0;
+
   }
 }
