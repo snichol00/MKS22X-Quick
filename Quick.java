743899c51c1a87535addfecf4feb9a954e54c159
Sophie Nichol <snichol00@stuy.edu>
2019-03-13 00:23:54 -0400

    fixed length errors in partition

--- Quick.java
+++ Quick.java
@@ -3,20 +3,47 @@ import java.util.Random;
 import java.lang.Math;
 
 public class Quick{
-  public class Partition{
     public static int partition(int [] data, int start, int end){
       //choose random pivot
       Random r = new Random();
       int pIdx = r.nextInt(data.length);
       int pVal = data[pIdx];
 
+      if (data.length == 0){
+        throw new IllegalArgumentException();
+      }
+      if (start == end){
+        return start;
+      }
+
+      if (data[start] > data[end] &&
+          data[start] < data[(end + start) / 2] || 
+          data[start] < data[end] &&
+          data[start] > data[(end + start) / 2]){
+        pVal = data[start];
+        pIdx = start;
+      }
+      else if (data[end] > data[start] &&
+              data[end] < data[(end + start) / 2] ||
+              data[end] < data[start] &&
+              data[end] > data[(end + start) / 2]){
+        pVal = data[end];
+        pIdx = end;
+      }
+      else{
+        pIdx = (end + start) / 2;
+      }
       //swap pivot to be first int
-      data[pIdx] = data[start];
-      data[start] = pVal;
+      if (start != pIdx){
+        data[pIdx] = data[start];
+        data[start] = pVal;
+        pIdx = start;
+        start ++;
+      }
       //System.out.println(Arrays.toString(data));
       //once start and end are equal, array has been partitioned
       while (start != end){
-        if (data[start] > pVal){
+        if (data[start] > pVal || data[start] == pVal){
           //swap start and end
           int temp = data[start];
           data[start] = data[end];
@@ -30,7 +57,11 @@ public class Quick{
       //System.out.println("loop finished");
       //System.out.println(Arrays.toString(data));
       //once sorted, you need to switch the pivot back into place
-      if (data[start] > pVal){
+      if (start >= data.length){
+        start--;
+      }
+
+      if (data[start] >= pVal){
         data[0] = data[start - 1];
         data[start - 1] = pVal;
         return start - 1;
@@ -48,10 +79,10 @@ public class Quick{
     int end = p;
     while (k != p){
       if (p < k){
-        start++;
+        start = p + 1;
       }
-      if (data[x] != prev){
-        end--;
+      else{
+        end = p - 1;
       }
       p = partition(data, start, end);
     }
