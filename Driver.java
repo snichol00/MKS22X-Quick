e3f5f95252cf414f772ad91b489d052fb1b91d3f
Sophie Nichol <snichol00@stuy.edu>
2019-04-02 06:42:03 -0400

    K's Driver

--- Driver.java
+++ Driver.java
@@ -1,54 +1,81 @@
 import java.util.Random;
+import java.util.Arrays;
 public class Driver{
-	public static void fillRandom(int[] data){
-	    Random r = new Random();
-	    int index = 0;
-	    for(int i = 0; i < data.length; i++){
-	      data[i] =  i; //r.nextInt(data.length); fills with random numbers
-	    }
-	    for (int i = 0; i < data.length; i++){
-	      int copy = data[i];
-	      int rand = r.nextInt(data.length);
-	      data[i] = data[rand];
-	      data[rand] = copy;
-	    }
-	 }
-	public static void printArray(int[] data){
-		String out = "[";
-		for(int i : data){
-			out+=i;
-			if (i != data[data.length-1]){
-				out+=", ";
-			}
-		}
-		System.out.println(out+"]");
-	}
-	public static void main(String[] args){
-		Random r = new Random(); int sucesses = 0;
-		int tests = 401; System.out.println("testing array sizes 1-400 having numbers from 0-n, asking for a random smallest int. (arrays do not contain duplicates)");
-		for (int i = 1; i < tests; i++){
-			int[] data = new int[i];
-			int select = i <= 1 ? 0 : r.nextInt(i);
-			fillRandom(data);
-			//printArray(data);                                            |
-			int result = Quick.quickselect(data, select); //sucess message V
-			if (result == select) {++sucesses;}//System.out.println("your array of size "+i+" sucessfully found the "+select+"th smallest one"); ++sucesses;}
-			else {
-				if (data.length < 40) {System.out.print("final Array: "); printArray(data);System.out.println();}
-				System.out.println("Uh Oh, your array of size "+i+" DID NOT find the "+select+"th smallest one. It returned " + result +" instead of " + select);
-			}
-		}
-		System.out.println("******************************************");
-		System.out.println("***testing with duplicates in the array***");
-		tests++; int[] data = new int[10000];
-		for (int i = 0; i < 100; i++){data[i] = r.nextInt(2);} Quick.quickselect(data,500); Quick.quickselect(data,99);
-		if (Quick.quickselect(data,0) == 0){++sucesses;} //chance of this driver failing here ;)
-		else {System.out.println("uh oh a large array of ones and zeros did not work");}
-		System.out.println("******************************************");
 
-		System.out.println("You have had " + sucesses + " sucesses and " + (tests-sucesses-1) + " failures.");
-		System.out.println( (sucesses == tests-1 ? "CONGRATULATIONS!!!" : "Uh Oh")+" Thats " + (sucesses / (tests-1.0)) * 100 + "% sucess rate.");
-		if (sucesses <= tests-100) {System.out.println("Yikes!!! Maybe he won't test some of those cases right?");}
-		else if (sucesses < tests-1) {System.out.println("Maybe if you compile and run again it will work.");}
-  	}
+ //Sort testing code
+  private static final int INCREASE = 0;
+  private static final int DECREASE = 1;
+  private static final int STANDARD = 2;
+  private static final int SMALL_RANGE = 3;
+
+  private static String name(int i){
+    if(i==INCREASE)return "Increassing";
+    if(i==DECREASE)return "Decreassing";
+    if(i==STANDARD)return "Normal Random";
+    if(i==SMALL_RANGE)return "Random with Few Values";
+    return "Error categorizing array";
+
+  }
+
+  private static int create(int min, int max){
+    return min + (int)(Math.random()*(max-min));
+  }
+
+  private static int[]makeArray(int size,int type){
+    int[]ans =new int[size];
+    if(type == STANDARD){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-1000000,1000000);
+      }
+    }
+    else if(type == INCREASE){
+      int current = -5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current += 10;
+      }
+    }
+    else if(type == DECREASE){
+      int current = 5 * size;
+      for(int i = 0; i < size; i++){
+        ans[i]= create(current,current + 10);
+        current -= 10;
+      }
+    }
+    else if(type == SMALL_RANGE){
+      for(int i = 0; i < size; i++){
+        ans[i]= create(-5,5);
+      }
+    }
+    else{
+      ans = new int[0];//empty is default
+    }
+    return ans;
+  }
+
+  public static void main(String[]args){
+    if(args.length < 2)return;
+
+    int size =  Integer.parseInt(args[0]);
+    int type =   Integer.parseInt(args[1]);
+
+    int [] start = makeArray(size,type);
+    int [] result = Arrays.copyOf(start,start.length);
+    Arrays.sort(result);
+
+    long startTime = System.currentTimeMillis();
+    /*
+     * Test your sort here //yoursort(start);
+     * Add code to switch which sort is tested by changing one of the args!
+     */
+		Quick.quicksort(start);
+    long elapsedTime = System.currentTimeMillis() - startTime;
+		System.out.println(Arrays.toString(result));
+		System.out.println(Arrays.toString(start));
+    if(Arrays.equals(start,result)){
+      System.out.println("PASS Case "+name(type)+"\t array, size:"+start.length+"\t"+elapsedTime/1000.0+"sec ");
+    }else{
+      System.out.println("FAIL ! ERROR ! "+name(type)+" array, size:"+size+"  ERROR!");
+    }
+  }
 }
